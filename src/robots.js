5750ae9550857760664c658fb17ab4c494eb4d33
Russell Longstaff <24914431+rllongstaff@users.noreply.github.com>
2018-09-06 11:02:15 +0100

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Maximillian',
+        username: 'Maxy',
+        email: 'maxy@blackhole.com'
+    },
+    {
+        id: 2,
+        name: 'R2D2',
+        username: 'Artoo',
+        email: 'artoo@tatooine.com'
+    },
+    {
+        id: 3,
+        name: 'C3P0',
+        username: 'Threepio',
+        email: 'threepio@anakinsinventions.com'
+    },
+    {
+        id: 4,
+        name: 'Johnny Five',
+        username: 'Five',
+        email: 'five@shortcircuit.net'
+    },
+    {
+        id: 5,
+        name: 'Wall-e',
+        username: 'Wally',
+        email: 'wally@compactordisposals.com'
+    },
+    {
+        id: 6,
+        name: 'Huey',
+        username: 'Drone1',
+        email: 'drone-1@silent-running.com'
+    },
+    {
+        id: 7,
+        name: 'Dewey',
+        username: 'Drone2',
+        email: 'drone-2@silent-running.com'
+    },
+    {
+        id: 8,
+        name: 'Louie',
+        username: 'Drone3',
+        email: 'drone-3@silent-running.com'
+    },
+    {
+        id: 9,
+        name: 'Robot',
+        username: 'Utility',
+        email: 'robot@lostinspace.com'
+    }                              
+]
